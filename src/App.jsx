00ebf01fd3dca2f9dee67001af207d1a9bf73d28
Francis Wilkinson <2400francis@gmail.com>
2024-03-14 14:24:25 +0000

    Added starter team-page files

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,28 @@ import './App.css';
 //import Navbar from './components/Navbar';
 //import Header from './components/Header';
 //import About from './pages/About';
-//import Team from './pages/Team';
-//import Home from './pages/Home';
+import Team from './pages/Team';
+import Home from './pages/Home';
 //import Jobs from './pages/Jobs';
 //import Footer from './components/Footer';
 
 function App() {
-  return //(
-  //  <Router>
-  //    <Navbar />
-  //    <Header />
+  return (
+    <Router>
+  {/* <Navbar /> */}
+  {/* <Header /> */}
      {/* Wraps Route elements in a Routes component */}
-  //    <Routes>
+    <Routes>
       {/* Routes using the Route component to render different page components at different paths */}
- //       <Route path="about" element={<About />} />
-  //      <Route path="team" element={<Team />} />
-  //      <Route path="/" element={<Home />} /> {/* Route that will render the Home component */}
+     {/* <Route path="about" element={<About />} /> */}
+     <Route path="Team" element={<Team />} />
+     <Route path="/" element={<Home />} />  {/* Route that will render the Home component */}
         {/* Route that will have descendant routes */}
-  //      <Route path="jobs/*" element={<Jobs />} />
-  //    </Routes>
-  //    <Footer />
-  //  </Router>
- // );
+      {/* <Route path="jobs/*" element={<Jobs />} /> */}
+    </Routes>
+   {/*<Footer />*/}
+  </Router>
+  );
 }
 
 export default App;
