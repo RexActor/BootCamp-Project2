888f00bd7b260bd7d533f661d717474667e40738
Qingqing Yang <qingqingyang@hotmail.co.uk>
2024-03-14 13:48:24 +0000

    add content to footer and tailwind script to html

--- src/App.jsx
+++ src/App.jsx
@@ -7,25 +7,21 @@ import './App.css';
 //import Team from './pages/Team';
 //import Home from './pages/Home';
 //import Jobs from './pages/Jobs';
-//import Footer from './components/Footer';
+import Footer from './components/Footer';
 
 function App() {
-  return //(
-  //  <Router>
-  //    <Navbar />
-  //    <Header />
-     {/* Wraps Route elements in a Routes component */}
-  //    <Routes>
-      {/* Routes using the Route component to render different page components at different paths */}
- //       <Route path="about" element={<About />} />
-  //      <Route path="team" element={<Team />} />
-  //      <Route path="/" element={<Home />} /> {/* Route that will render the Home component */}
-        {/* Route that will have descendant routes */}
-  //      <Route path="jobs/*" element={<Jobs />} />
-  //    </Routes>
-  //    <Footer />
-  //  </Router>
- // );
+      return (
+            <Router>
+      {/* <Navbar /> */}
+      {/* <Routes>  
+       <Route path="about" element={<About />} />
+        <Route path="team" element={<Team />} />
+        <Route path="/" element={<Home />} />        
+        <Route path="jobs/*" element={<Jobs />} />
+      {/* </Routes> */}
+                  <Footer />
+            </Router>
+      );
 }
 
 export default App;
