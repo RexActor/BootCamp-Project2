7d368eeff55f69cdac0ee4e8454dae77ff32fc65
Gatis Jansons <bobsters.lol@gmail.com>
2024-03-14 19:52:43 +0000

    added feature to read API from Adzuna and created boilerplate for search form

---
+++ src/components/SearchForm.jsx
@@ -0,0 +1,30 @@
+function SearchForm(props) {
+  return (
+    <form>
+      <div className="grid gap-6 mb-6 md:grid-cols-2">
+        <div>
+          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+            Search:
+          </label>
+          <input
+            onChange={props.handleInputChange}
+            value={props.search}
+            name="search"
+            type="text"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Search for job"
+            id="search"
+          />
+        </div>
+        <button
+          onClick={props.handleFormSubmit}
+          type="submit"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Search
+        </button>
+      </div>
+    </form>
+  );
+}
+export default SearchForm;
