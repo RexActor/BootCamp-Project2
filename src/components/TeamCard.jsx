ace4cebdcf5944f21ed488338182e2d6e0d78aa2
Qingqing Yang <qingqingyang@hotmail.co.uk>
2024-03-18 18:29:57 +0000

    add more content to json file and teamcard file

--- src/components/TeamCard.jsx
+++ src/components/TeamCard.jsx
@@ -3,30 +3,64 @@ import React from "react";
 
 function TeamCard(props) {
   return (
-<div className="card max-w-sm rounded overflow-hidden shadow-lg">
-    <div className="w-full">
-        <div className="img-container">
-        <img className="mx-auto md:text-center"
-        alt= {props.name}
-        src= {props.image}
+
+    <div className="flex flex-col items-center rounded-lg bg-gray-100 p-4 lg:p-8">
+      {/* Person photo */}
+      <div className="mb-2 h-24 w-24 overflow-hidden rounded-full bg-gray-200 shadow-lg md:mb-4 md:h-32 md:w-32">
+        <img className="h-full w-full object-cover object-center"
+          alt={props.name}
+          src={props.image}
         />
-        </div>
-      <div className="content px-6 py-4 pb-2">
-      <ul className="list-disc list-outside marker:text-amber-600">
-        <li className="text-gray-600 font-bold text-xl mb-2">
-          {props.name}
-        </li>
-        <li>
-          <a className="text-indigo-500 font-bold" href={props.portfolio}>Portfolio site</a>
-        </li>
-        <li>
-          <a className="text-indigo-500 font-bold" href={props.github}>Github repo</a>
-        </li>
-      </ul>
       </div>
+      {/* Person */}
+      <div className="text-center">
+        {/* Person infor */}
+        <h3 className="font-bold text-gray-800 md:text-lg">
+          {props.name}
+        </h3>
+        <p className="my-2 text-gray-600 rounded-lg bg-gray-200 inline-block py-1 px-2 text-sm font-medium">
+          {props.title}
+        </p>
+        <p className="mb-3 text-gray-500 md:mb-4 md:text-base"
+        >
+          {props.duty}
+        </p>
+
+        {/* social links*/}
+        <div className="flex justify-center">
+          <div className="flex items-center gap-4">
+            {/* Linkedin */}
+            <a
+              href={props.linkedin}
+              target="_blank"
+              className="text-indigo-500 transition duration-100 hover:text-gray-500 active:text-gray-600">
+              <span className="sr-only">Linkedin</span>
+              <svg className="h-5 w-5" width="24" height="24" viewBox="0 0 24 24"
+                fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
+              </svg>
+            </a>
+            {/* GitHub */}
+            <a
+              href={props.github}
+              target="_blank"
+              class="text-indigo-500 transition duration-100 hover:text-gray-500 active:text-gray-600">
+              <span class="sr-only">GitHub</span>
+              <svg class="h-6 w-6" width="24" height="24" viewBox="0 0 24 24"
+                fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" />
+              </svg>
+            </a>
+          </div>
+        </div>
+
+      </div>
+
     </div>
-</div>
-    
+
+
   );
 }
 
--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -4,18 +4,39 @@ import TeamMembers from "../utils/TeamMembers.json";
 
 function Team() {
   return (
-    <div className='container pt-6'>
-      <div className='flex flex-row flex-wrap w-400 space-x-4 justify-center'>
-       {TeamMembers.map((TeamMembers) => (
-        <TeamCard
-          name={TeamMembers.name}
-          image={TeamMembers.image}
-          portfolio={TeamMembers.portfolio}
-          github={TeamMembers.github}
-        />
-      ))}
-      </div>
-    </div>
+    <main id="team-list" className="px-8 py-8 bg-gray-100 text-gray-800">
+      <section className="mx-auto max-w-screen-lg flex justify-between items-center">
+        <div className="mx-auto max-w-screen-xl px-4 md:px-8">
+          {/* Heading */}
+          <div className="mb-10 md:mb-16">
+            <h1 className="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl">
+              Meet our Team
+            </h1>
+            <p className="mx-auto max-w-screen-md text-center text-gray-600 md:text-lg">
+              Welcome to the Pathway team! Our dedicated and talented individuals work collaboratively to
+              bring you the best product.
+            </p>
+          </div>
+
+          {/* Team cards */}
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:gap-8">
+            {TeamMembers.map((TeamMember) => (
+              <TeamCard
+                key={TeamMember.id}
+                id={TeamMember.id}
+                image={TeamMember.image}
+                portfolio={TeamMember.portfolio}
+                name={TeamMember.name}
+                title={TeamMember.title}
+                duty={TeamMember.duty}
+                linkedin={TeamMember.linkedin}
+                github={TeamMember.github}
+              />
+            ))}
+          </div>
+        </div>
+      </section>
+    </main>
   );
 }
 
