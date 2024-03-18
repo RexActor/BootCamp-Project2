65b4e35141a0d29a11a859c1c69149e58c74ef17
sumzulfikar <sumaiya.zulfikar29@gmail.com>
2024-03-18 11:42:09 +0000

    Load more button added for job results with 4 displays

--- src/pages/Jobs.jsx
+++ src/pages/Jobs.jsx
@@ -1,47 +1,30 @@
 import React, { useState, useEffect } from "react";
 import JobCard from "../components/JobCard";
-import jobData from "../utils/data.json";
 import { useLocation } from "react-router-dom";
 
 const Jobs = () => {
-  const { state } = useLocation();
+    const { state } = useLocation();
 
-  const [jobCards, setJobCards] = useState(state.results);
-  const [visibleCards, setVisibleCards] = useState(4);
-  const cardsPerPage = 4;
-  const loadMoreCards = () => {
-    setVisibleCards((prevVisibleCards) => prevVisibleCards + cardsPerPage);
-  };
-  useEffect(() => {
-    setJobCards(state.results);
-  }, []);
+    const [jobCards, setJobCards] = useState(state.results);
+    const [visibleCards, setVisibleCards] = useState(4);
+    const cardsPerPage = 4;
 
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, [visibleCards]);
+    useEffect(() => {
+        setJobCards(state.results);
+    }, []);
 
-  const handleScroll = () => {
-    if (
-      window.innerHeight + document.documentElement.scrollTop >=
-        document.documentElement.offsetHeight - 20 &&
-      visibleCards < jobCards.length
-    ) {
-      loadMoreCards();
-    }
-  };
 
-  return (
-    <div>
-      <div>
-        {jobCards.slice(0, visibleCards).map((job, index) => (
-          <JobCard id={job.id} key={job.id} job={job} data={jobCards} />
-        ))}
-      </div>
+    return (
 
-      <button onClick={loadMoreCards}>Load More Jobs</button>
-    </div>
-  );
+
+        <div>
+            {jobCards.map((job, index) => (
+                <JobCard id={job.id} key={job.id} job={job} data={jobCards} />
+            ))}
+        </div>
+
+
+    );
 };
 
 export default Jobs;
