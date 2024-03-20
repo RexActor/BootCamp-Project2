82a6f3d7f945e7558c731ebd9a0449771e1bea76
SiminAnaraki <simin.anaraki@gmail.com>
2024-03-20 15:11:28 +0000

    improved modal appearance

--- src/pages/JobDetails.jsx
+++ src/pages/JobDetails.jsx
@@ -9,6 +9,8 @@ function JobDetails(props) {
     setHeightClass(newHeightClass);
   }, []);
 
+  const minSalary = (props.info.salary_min).toFixed(0).toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+  const maxSalary = (props.info.salary_max).toFixed(0).toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
   const calAvg = (((props.info.salary_min + props.info.salary_max) / 2)-8000).toFixed(0);
   const avg = calAvg.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
   return (
@@ -22,7 +24,7 @@ function JobDetails(props) {
               <p className='text-gray-700 text-md'>{props.info.company.display_name}</p>
               <p className='mb-3 text-gray-500 text-sm'>{props.info.location.display_name}</p>
               <div className='mb-3 '>
-              <span>{`£ ${props.info.salary_min} - ${props.info.salary_max}  a year`}</span> 
+              <span>{`£ ${minSalary} - ${maxSalary}  a year`}</span> 
               <span className='pl-5 uppercase'>{props.info.contract_time}</span>
             </div>
             <a href={props.info.redirect_url}  target="_black" className='mt-3 inline-block px-5 py-3 text-base font-medium text-center text-white rounded-full bg-gray-800 hover:bg-opacity-60 focus:ring-4 '>apply now</a>
@@ -50,7 +52,7 @@ function JobDetails(props) {
                 </svg>
                 <h4 className='ml-2  font-semibold'>Pay</h4>
               </div>
-              <p className='ml-8 my-3 bg-gray-200 p-1 rounded-md inline-block'>£ {props.info.salary_min}  -  £ {props.info.salary_max}</p>
+              <p className='ml-8 my-3 bg-gray-200 p-1 rounded-md inline-block'>£ {minSalary}  -  £ {maxSalary}</p>
               <div className="flex pt-4">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z" />
