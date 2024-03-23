311a8eb0b60e7dc799f86ff2002079be5861eb85
Gatis Jansons <13068778+RexActor@users.noreply.github.com>
2024-03-23 19:55:37 +0000

    Delete src/utils/APIKeys.js

--- src/utils/APIKeys.js
+++
@@ -1,7 +0,0 @@
-const appID = "b9900fa2";
-
-const apiKey = "546651ec2d56735011331769606df50a";
-const autoCompleteKey = "IOv1oSaJlK-5RRas0SYsM-7vBMmy28kUR0cbu5QIE4k";
-const googleMapKey = "AIzaSyBKHAQbo2RGG9JP5atgCvPX8AflO_wi8-Q";
-
-export default { appID, apiKey, autoCompleteKey, googleMapKey };
